91a5391bff9031dca5649dc26e4a435d0f6c410c
Lisa Qing <lisaqing98@gmail.com>
2021-04-28 12:08:25 -0700

    sampling code converging

--- sampling.py
+++ sampling.py
@@ -13,7 +13,12 @@ class Sampler(object):
 		self.Avar = matrix(0, self.D)
 		self.yvar = matrix(0, 1)
 		x = tt.vector()
-		self.f = th.function([x], -tt.sum(tn.relu(tt.dot(-tt.tile(self.yvar,[1,D])*self.Avar, x))))
+#		self.f = th.function([x], -tt.sum(tn.relu(tt.dot(-tt.tile(self.yvar,[1,D])*self.Avar, x))))
+
+	#cannot use theano function because x is not an array but a tensor variable
+	def f(self, x):
+		return -tt.sum(tn.relu(tt.dot(-tt.tile(self.yvar,[1,self.D])*self.Avar, x)))
+		
 	@property
 	def A(self):
 		return self.Avar.get_value()
@@ -36,29 +41,31 @@ class Sampler(object):
 
 	def sample(self, N, T=50, burn=1000):
 		with mc.Model() as model:
-			x = mc.Uniform(name='x', lower=-np.ones(self.D), upper=np.ones(self.D), shape=self.D, testval=np.zeros(self.D))
+			x = mc.Uniform(name='x', lower=-np.ones(self.D), upper=np.ones(self.D), shape=self.D, testval=np.zeros(self.D), transform=None)
 			
-			def sphere(x):
-				if(mc.math.ge(((x**2).sum()),1.)):
-					#Can't do -inf because it'll make sampling throw an error of "bad initial energy"
-					return np.iinfo(np.int64).min
-				else:
-					return self.f(x)
+#			def sphere(x):
+#				if(mc.math.ge(((x**2).sum()),1.)):
+#					return -np.iif
+#				else:
+#					return self.f(x)
+			
+			#potential examples: https://docs.pymc.io/notebooks/gaussian_mixture_model.html
+			#PYMC3 and Theano summary: https://docs.pymc.io/PyMC3_and_Theano.html
+			#sphere code - the previous method is completely different
+			test = (x**2).sum()
+			p1 = mc.Potential("sphere", tt.switch((test>1.).all(), -np.inf, self.f(x)))
 
-			#need to convert sphere to a theano tensor
-			p1 = mc.Potential(name="sphere", var=tt.constant(sphere(x)))
-
-			#No adaptive metropolis function in pymc3?
-			steps = mc.Metropolis(vars=x, delay=burn, cov=np.eye(self.D)/10000)
+			#mc.Metropolis in PYMC3 implements adaptive Metropolis-Hastings
+			steps = mc.Metropolis([x], delay=burn, cov=np.eye(self.D)/10000)
 		
-			#There aren't key word arguments for thin or burn? --> what to do?
-			#burn can be used to get the trace values? is that good enough?
-			#samples = pm.sample(N*T+burn, thin=T, burn=burn, verbose=-1)
-			#need cores definition otherwise i get a broken pipe?
-			#...no clude what tune is - needs a large number tune=(N*T+burn/2)?
-			trace = mc.sample(N*T+burn, tune=(N*T+burn/2), step=steps, cores=1)
-
-			samples = trace.get_values('x', burn=burn)
+			#sampling just requires, #samples, steps, and cores (I can't run more than 1 core otherwise I get a broken pipe issue)
+			#tune also seems to be needed with PYMC3 default value is too small
+			trace = mc.sample(N*T+burn, tune=(N*T+burn)/2, step=steps, cores=1, return_inferencedata=False)
+			
+			#we get selected samples from the trace with thin and burn parameters
+			samples = trace.get_values('x', thin=T, burn=burn)
+			
+			#normalize the samples
 			samples = np.array([x/np.linalg.norm(x) for x in samples])
 		
 			return samples
